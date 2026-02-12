48484ffb94eea7fcbcfd82ee378d20040638402b
Klingsterina <eyglo2001@gmail.com>
2026-02-12 22:52:25 +0000

    US9 komið hægt er að opna wishlist

--- app/src/main/java/is/hi/present/navigation/Routes.kt
+++ app/src/main/java/is/hi/present/navigation/Routes.kt
@@ -3,4 +3,6 @@ package `is`.hi.present.navigation
 object Routes {
     const val WISHLISTS = "wishlists"
     const val CREATE_WISHLIST = "create_wishlist"
+    const val WISHLIST_DETAIL = "wishlist/{wishlistId}"
+    fun wishlistDetail(wishlistId: String) = "wishlist/$wishlistId"
 }
